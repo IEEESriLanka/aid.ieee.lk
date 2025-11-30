8899e8d8f0f9c8c0ac5837581c2cc82234cb5544
Sachintha Wickramasinghe <69675289+sachinthadw@users.noreply.github.com>
2025-12-01 04:50:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // FIX: Use './' so it works on your GitHub repo path automatically
-  base: './', 
+  base: '/', // Cleaner for root domains like drf.ieee.lk
 })
