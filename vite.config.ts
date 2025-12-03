d00fe233a73fb2c9da2815ba864162ad626af6ca
Sachintha Wickramasinghe <69675289+sachinthadw@users.noreply.github.com>
2025-12-04 01:12:08 +0530

    Update alias resolution in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Maps '@' to the project root for imports
-      '@': path.resolve('./'),
+      '@': path.resolve(__dirname, './'),
     },
   },
 });
