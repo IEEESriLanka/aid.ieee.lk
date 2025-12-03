b28c90c7bd777f192b5f0b15e4e2bcbf343673b1
Sachintha Wickramasinghe <69675289+sachinthadw@users.noreply.github.com>
2025-12-04 01:05:40 +0530

    Error fix in paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: This ensures assets are loaded correctly on GitHub Pages (subdirectories)
+  base: './',
   resolve: {
     alias: {
       // Maps '@' to the project root for imports
